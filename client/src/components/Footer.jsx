b3aa3a469df38985fa09da405a271946aa16afa3
Vishal Kalita <vishalkalita036@gmail.com>
2024-10-04 19:43:43 +0530

    modified footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <div className="flex items-center bg-[#001b3a] md:h-20 h-16 text-[#80a2c8] text-xs justify-start px-14">
-      Copyright © 2015 - christuniversity.in
+    <div className="flex items-center bg-[#001b3a] md:h-20 h-16 text-[#80a2c8] text-base px-14">
+      <div className="flex-1 text-left">Copyright © 2015 - christuniversity.in</div>
+      <div className="flex-1 text-center">Concept and design by: Dr. Ashok Immanuel. V</div>
+      <div className="flex-1"></div>
     </div>
   );
 };
